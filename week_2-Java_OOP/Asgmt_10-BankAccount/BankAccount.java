40e3b2d835612dfbe895f65e957edb35b69886bc
Brittney Perez <brittney.mp2005@gmail.com>
2023-06-17 02:33:56 -0400

    finished BankAccount √

--- week_2-Java_OOP/Asgmt_10-BankAccount/BankAccount.java
+++ week_2-Java_OOP/Asgmt_10-BankAccount/BankAccount.java
@@ -1,16 +1,19 @@
+import java.text.DecimalFormat;
+
 public class BankAccount {
     
-    // * CLASS ATTRIBUTES
+    //* CLASS ATTRIBUTES
+    // Todo: Users should not be able to set any of the attributes from the class. √
+    // This means to make them "private".
     private double checkingBalance;
     private double savingsBalance;
     
-    // * CLASS MEMBER VARIABLES - static
+    //* CLASS MEMBER VARIABLES - static
     public static int numberOfAccounts;
-    // public double totalAmountOfMoney;
     public static double totalAmountOfMoney;
     
     
-    // * CONSTRUCTOR
+    //* CONSTRUCTOR
     // Todo: In the constructor, be sure to increment the account count.
     public BankAccount() { //? Upon creating of BankAccount, default values = 0
         BankAccount.numberOfAccounts += 1;
@@ -18,51 +21,56 @@ public class BankAccount {
         this.savingsBalance = 0;
     }
     
-    // * GETTERS * SETTERS
-    // Getter method for the user's checking account balance.
-    public double getCheckingBalance() {
-        return this.checkingBalance;
+    //* GETTERS & SETTERS
+    //? Getter methods for the user's checking and savings account balance.
+    public String getCheckingBalance() {
+        DecimalFormat decimalFormat = new DecimalFormat("#0.00");
+        return decimalFormat.format(this.checkingBalance);
     }
-    // Getter method for the user's saving account balance.
-    public double getSavingsBalance() {
-        return this.savingsBalance;
+    public String getSavingsBalance() {
+        DecimalFormat decimalFormat = new DecimalFormat("#0.00");
+        return decimalFormat.format(this.savingsBalance);
     }
     
+    //* METHODS
     // Todo: Create a method that will allow a user to deposit money into either the checking or saving, be sure to add to total amount stored.
     public void depositCheck( double amountDeposited, String accountName ) {
         if( accountName == "checking" ) {
             this.checkingBalance += amountDeposited;
+            System.out.printf("You deposited $%.2f to your %s account.\n", amountDeposited, accountName);
         } else {
             this.savingsBalance += amountDeposited;
+            System.out.printf("You deposited $%.2f to your %s account.\n", amountDeposited, accountName);
         }
-        this.totalAmountOfMoney += amountDeposited;
+        BankAccount.totalAmountOfMoney += amountDeposited;
     }
     // Todo: Create a method to withdraw money from one balance. Do not allow them to withdraw money if there are insufficient funds.
-    public void withdraw( double amountWithdrawed, String accountName ) {
-        if( accountName == "checking" ) {
-            //! Warn user that there is insufficient balance
-            if( amountWithdrawed > this.checkingBalance ) {
-                System.out.println("Insufficient Funds!");
-                return;
-            }
-            this.checkingBalance -= amountWithdrawed;
+    private boolean hasSufficientFunds( double amount, String accountName ) {
+        if(accountName.equals("checking")) { //? Check if there's an account obj called "checking"
+            return amount <= this.checkingBalance;
         } else {
-            if( amountWithdrawed > this.savingsBalance ) {
-                System.out.println("Insufficient Funds!");
-                return;
-            }
-            this.savingsBalance -= amountWithdrawed;
+            return amount <= this.savingsBalance;
         }
-        this.totalAmountOfMoney -= amountWithdrawed;
     }
-    // Todo: Create a method to see the total money from the checking and saving.
-    // Todo: Users should not be able to set any of the attributes from the class.
     
+    public void withdraw( double amountWithdrawed, String accountName ) {
+        if( !hasSufficientFunds(amountWithdrawed, accountName) ) {
+            System.out.println("Insufficient Funds!");
+            return;
+        }
+        if(accountName.equals("checking")) {
+            this.checkingBalance -= amountWithdrawed;
+            System.out.printf("You withdrawed $%.2f from your %s account.\n", amountWithdrawed, accountName);
+        } else {
+            this.savingsBalance -= amountWithdrawed;
+            System.out.printf("You withdrawed $%.2f from your %s account.\n", amountWithdrawed, accountName);
+        }
+        BankAccount.totalAmountOfMoney -= amountWithdrawed;
+    }
     
-    
-    
-    
-    // Todo: depositCheck()
-    // Todo: checkBalance()
-    // Todo: withdraw()
+    // Checks the total money from the checking and saving. √
+    public void checkBalance() {
+        DecimalFormat decimalFormat = new DecimalFormat("#0.00");
+        System.out.println("Total Balance: $" + decimalFormat.format(totalAmountOfMoney) + "\n");
+    }
 }
--- week_2-Java_OOP/Asgmt_10-BankAccount/TestBankAccount.java
+++ week_2-Java_OOP/Asgmt_10-BankAccount/TestBankAccount.java
@@ -9,15 +9,24 @@ public class TestBankAccount {
         testBankAccount.depositCheck(200, "savings");
         System.out.println("Test Checking Balance: $" + testBankAccount.getCheckingBalance());
         System.out.println("Test Savings Balance: $" + testBankAccount.getSavingsBalance());
-        System.out.println("Total Balance: $" + testBankAccount.totalAmountOfMoney + "\n");
+        testBankAccount.checkBalance();
+        /* Long-Hand Version
+            Without this method, it'll be written as...
+            System.out.println("Total Balance: $" + testBankAccount.totalAmountOfMoney + "\n");
+        */
         
         testBankAccount.withdraw(90, "checking");
         testBankAccount.withdraw(25, "savings");
         // System.out.println("Test Checking Balance: $" + testBankAccount.getCheckingBalance());
         // System.out.println("Test Savings Balance: $" + testBankAccount.getSavingsBalance());
+        
         // System.out.println("Total Balance: $" + testBankAccount.totalAmountOfMoney);
+        //? the above line would work if I used this.totalAmountOfMoney in the deposit/withdraw methods
         
         testBankAccount.withdraw(200, "savings");
         System.out.println("Test Savings Balance: $" + testBankAccount.getSavingsBalance());
+        
+        // Check total money from the checking and savings:
+        testBankAccount.checkBalance();
     }
 }
