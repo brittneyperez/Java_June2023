5cbb6e6d1c44626dd65100efbdb0c4d3b88428f4
Brittney Perez <brittney.mp2005@gmail.com>
2023-06-22 03:42:02 -0400

    updated spring boot notes

--- week_3-Java_Spring/Guided_Readings-Concepts/Spring_Intro/helloWorld/src/main/java/com/codingdojo/helloworld/HomeController.java
+++ week_3-Java_Spring/Guided_Readings-Concepts/Spring_Intro/helloWorld/src/main/java/com/codingdojo/helloworld/HomeController.java
@@ -1,6 +1,8 @@
 package com.codingdojo.helloworld;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -33,4 +35,18 @@ public class HomeController {
 		return "再见。Goodbye world!";
 	}
     
+	// Query Parameters ----------------
+	@RequestMapping("/search")
+	public String index( @RequestParam(value="q", required=false) String searchQuery ) { // ex: localhost:8080/hello/search?q=coding+dojo
+		if( searchQuery == null ) {
+			return "You searched for nothing.";
+		}
+		return "You've searched for: " + searchQuery;
+	}
+	
+	// URL & @PathVariables ------------
+	@RequestMapping("/game/{userID}/{userName}")
+    public String showGameInfo(@PathVariable("userID") String userID, @PathVariable("userName") String userName){
+		return "UID: " + userID + ", UserName: " + userName;
+    }
 }
